0e815f632fa1ddbc06045b7e2f7f9bee1a6a127a
imsejin <hackeres123@gmail.com>
2025-04-28 15:36:55 +0900

    feat: 예외 클래스 수정

--- src/main/java/kr/hhplus/be/server/common/exception/BusinessException.java
+++ src/main/java/kr/hhplus/be/server/common/exception/BusinessException.java
@@ -1,20 +1,24 @@
 package kr.hhplus.be.server.common.exception;
 
 import lombok.Getter;
+import org.jetbrains.annotations.Nullable;
 
 @Getter
 public class BusinessException extends RuntimeException {
 
     private final BusinessError businessError;
 
+    @Nullable
+    private final Object data;
+
     public BusinessException(BusinessError businessError) {
-        super(businessError.getMessage());
-        this.businessError = businessError;
+        this(businessError, null);
     }
 
-    public BusinessException(BusinessError businessError, Throwable cause) {
-        super(businessError.getMessage(), cause);
+    public BusinessException(BusinessError businessError, @Nullable Object data) {
+        super(businessError.getMessage());
         this.businessError = businessError;
+        this.data = data;
     }
 
 }
